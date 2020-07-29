1babfccce86a54cece7ddae9f0c30253f56640b7
matheusbez <matheus_isidoro@hotmail.com>
2020-07-29 02:51:09 -0300

    tarefa forms ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'anunciar',
+    loadChildren: () => import('./anunciar/anunciar.module').then( m => m.AnunciarPageModule)
   }
 ];
 @NgModule({
